36830661d8f5516aeee6aa13630fbb764182840c
iryna-zatynina <ira.zat1997@gmail.com>
2023-05-30 18:16:53 +0200

    add title adoptation

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Title from "../../views/Title/Title";
-import Container from "../../components/Container/Container";
 import './Main.scss';
 
 const Main = () => {
     return (
         <main className="main">
-            <Container>
-                <Title />
-            </Container>
+            <Title />
         </main>
     );
 };
--- src/views/Title/Title.tsx
+++ src/views/Title/Title.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Title.scss';
+import Container from "../../components/Container/Container";
 
 const Title = () => {
     return (
         <section className="title">
-            <h1>Iryna Zatynina</h1>
-            <p>Front End Developer</p>
+            <Container>
+                <h1>Iryna Zatynina</h1>
+                <p>Front End Developer</p>
+            </Container>
         </section>
     );
 };
