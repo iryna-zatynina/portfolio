395e42ea6c81270ba98b2b7a77a0793190849910
iryna-zatynina <ira.zat1997@gmail.com>
2023-08-10 19:48:17 +0200

    add footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from "../../layout/Header/Header";
 import Main from "../../layout/Main/Main";
+import Footer from '../../layout/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Main />
+        <Footer />
     </div>
   );
 }
---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>© 2023 Iryna Zatynina</footer>
+  )
+}
+
+export default Footer;
